f64386d6991d77dba81b0f2754640d97106c3e40
juditanita <101408621+juditanita@users.noreply.github.com>
2024-11-04 18:04:08 +0100

    array of objects

--- demo/array-object/array-object.ts
+++ demo/array-object/array-object.ts
@@ -1,3 +1,29 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
 
+type Link={
+    title:string,
+    url:string
+}
+
+const microsoft ={
+    title:"microsoft",
+    url:"https://www.microsoft.com"
+}
+
+
+const typescript ={
+    title:"typescript",
+    url:"https://www.typescript.com"
+}
+
+
+const links: Link[] = [microsoft, typescript]
+
+const tsurl = links[1].title
+
+links[0].title = "Microsoft"
+
+links.forEach(item=>{
+    console.log(item);
+})

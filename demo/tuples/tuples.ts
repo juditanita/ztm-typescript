007ab085bcb91f96fb29f8f02274b31876ce8a54
juditanita <101408621+juditanita@users.noreply.github.com>
2024-11-04 18:21:06 +0100

    tuples

--- demo/tuples/tuples.ts
+++ demo/tuples/tuples.ts
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
 
+
 // Tuples provide a way to express an array with a fixed number of elements of
 // different types, creating a data structure with multiple different types.
 // They can be especially handy when dealing with scenarios such as
@@ -11,3 +12,33 @@ import { strict as assert } from "assert";
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/2/objects.html#tuple-types
 //
+
+
+type Title = string;
+type PublisedYear = number;
+
+type Book = [Title, PublisedYear]
+
+const sample: Book =["new year hope", 2078]
+
+const bookTitle:Title = "New Year Hope";
+assert.strictEqual(sample[0].toUpperCase(), bookTitle.toUpperCase());
+
+function coordinates():[number, number]{
+    return[3,5]
+}
+
+
+const [x,y ]= coordinates();
+assert.equal(x,3)
+assert.equal(y,5)
+
+
+const mulitTuple :[number, number][]=[
+    coordinates(),
+    [1,9],
+    [3,4],
+    [2,1]
+]
+
+

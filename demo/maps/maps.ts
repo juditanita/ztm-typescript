f541918c8b0a17dfb9d98343f7e3b0301c81c211
juditanita <101408621+juditanita@users.noreply.github.com>
2024-11-07 17:18:32 +0100

    map iterations

--- demo/maps/maps.ts
+++ demo/maps/maps.ts
@@ -11,4 +11,43 @@ import { strict as assert } from "assert";
 //
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
-//
+
+type Name = string;
+type Score = number;
+//                     key, value
+const testScore : Map<Name, Score> = new Map();
+
+testScore.set("Alice",98);
+testScore.set("Bob",38);
+testScore.set("Teddy",60);
+
+
+assert.equal(testScore.size, 3);
+
+
+  let nameDeleted;
+for (const [name, score] of testScore){
+ 
+if(score < 40){
+       nameDeleted = name;
+       testScore.delete(nameDeleted);   
+    console.log(`${nameDeleted} needs to do the exam again.`);
+    }
+    else if(score > 90){
+        console.log(`Well done ${name}.`)
+    } else{
+        console.log(`${name} has ${score}.`)
+
+    }
+
+}
+
+assert.equal(testScore.has("Mark"),false);
+
+for ( const name of testScore.keys()){
+    console.log(name)
+}
+
+for ( const score of testScore.values()){
+    console.log(score)
+}
